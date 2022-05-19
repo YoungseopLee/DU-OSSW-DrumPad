6ee7eb74928233121710fa8bc191866a2ec3e6ab
jhlee <zergling144@naver.com>
2022-05-19 17:53:19 +0900

    hotfix & create m4a file

--- metronome.js
+++ metronome.js
@@ -1,40 +1,49 @@
-
-
-
-
-
-
+const drum = new Howl({
+    "src": [
+        "metronome.m4a"
+    ],
+    "sprite" : {
+        "metronome" : [
+            1250,
+            553.0839002267571
+        ]
+    }
+});
 
 function Timer(callback, timeInterval, errorCallback) {
     this.timeInterval = timeInterval;
     this.start = () => {
         this.expected = Date.now() + this.timeInterval;
         this.timeout = setTimeout(this.round, this.timeInterval);
-        console.log(Started);
+        console.log('started');
     }
+
     this.stop = () => {
         clearTimeout(this.timeout);
         console.log('stopped');
+        
     }
+
     this.round = () => {
-        let drift = DATE.NOW() - this.expected;
+        let drift = Date.now() -this.expected;
         if (drift > this.timeInterval) {
             if (errorCallback) {
-                errorCallback;
+                errorCallback();
             }
-        }        
+        }
         callback();
         this.expected += this.timeInterval;
         console.log(drift);
         console.log(this.timeInterval - drift);
-        this.timeout = setTimout(this.round, this.timeInterval - drift);
+        this.timeout = setTimeout(this.round, this.timeInterval - drift);
     }
 }
-const kick = new Timer(() => {drums.play('kick')} ,500);
+const kick = new Timer(() => {drum.play('metronome')}, 500);
 
-function startTest() {
+function Teststart() {
     kick.start();
 }
-function stopTest() {
+
+function Teststop() {
     kick.stop();
 }
